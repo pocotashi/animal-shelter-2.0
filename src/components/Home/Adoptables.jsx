5a7d0ddb6872f9ed0ef284bfbdcbd601999fe388
Chosum Tashi <tashi.chosum@gmail.com>
2021-11-21 19:57:20 -0500

    make responsive: navbar, firstsection, adoptables, myth

--- src/components/Home/Adoptables.jsx
+++ src/components/Home/Adoptables.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import Nav from 'react-bootstrap/Nav';
+import { Container, Row, } from 'react-bootstrap';
+
 
 
 
 function Adoptables(){
     return (
-    <div className="adoptables">
-    <Container >
-    <Row className="text-adopt">
-    <Col><Nav.Link className="adopt-text" href="/adoptabledogs"><h1 >ADOPTABLE DOGS</h1>  </Nav.Link></Col>
-    <Col><h1 >|</h1></Col>
-    <Col><Nav.Link className="adopt-text" href="/adoptablecats"><h1 >ADOPTABLE CATS</h1>  </Nav.Link></Col>
-  </Row>
-  </Container>
+      <div className="adoptables ">
+        <Container>
+          <Row className="text-adopt">
+          <span class="left"><a href="/adoptabledogs">ADOPTABLE DOGS</a></span>
+          <span class="right"><a href="/adoptablecats">ADOPTABLE CATS</a></span>
+          </Row>
+        </Container>
     </div>
     )
 }
