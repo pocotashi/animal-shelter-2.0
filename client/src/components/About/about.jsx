79de4c9746eb79823e947c7177c44a853f47573e
Chosum Tashi <tashi.chosum@gmail.com>
2021-12-02 19:12:06 -0500

    Connect to MongoDB

--- client/src/components/About/about.jsx
+++ client/src/components/About/about.jsx
@@ -13,8 +13,8 @@ function About(){
     <BodySection
       header={'About US'}
       message={'The mutual love for animals brought us together. The will to help the voiceless propels us forward.'}
-      para={'Sarama Animal Rescue was inspired by the animals that need us most. They have taught us about unconditional love, about trusting even when you’re vulnerable and about resilience and hope even after trauma. This rescue is our way to honor them and to give back to them.'}
-      para1={'Before creating Sarama Animal Rescue, we worked in the animal rescue world for many years. We wanted to do more for all of the animals we came across that were the most often overlooked for adoption, whether because of behavioral issues, struggling in the stressful environment of a kennel, or even just their physical appearance. We couldn’t help but put ourselves in the animals’ shoes and think how we would feel to be abandoned by ones we loved, end up in a stressful shelter and get overlooked time and time again.'}
+      para={'Kipu\'s Rescue was inspired by the animals that need us most. They have taught us about unconditional love, about trusting even when you’re vulnerable and about resilience and hope even after trauma. This rescue is our way to honor them and to give back to them.'}
+      para1={'Before creating Kipu\'s Rescue, we worked in the different fields for many years. We wanted to do more for all of the animals we came across that were the most often overlooked for adoption, whether because of behavioral issues, struggling in the stressful environment of a kennel, or even just their physical appearance. We couldn’t help but put ourselves in the animals’ shoes and think how we would feel to be abandoned by ones we loved, end up in a stressful shelter and get overlooked time and time again.'}
       para2={'Once taken out of the shelter environment and placed in a foster home, many of these animals thrived and became well-adjusted members of the family. While there are some that do need extra TLC, we have never regretted giving an animal a chance.'}
 
     />
